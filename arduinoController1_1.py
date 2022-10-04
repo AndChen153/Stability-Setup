1f96b77d55ea54420b8ab456546d47b569c5ce33
Andrew Chen <achen1192@gmail.com>
2022-10-04 10:39:47 -0400

    updated sendtoarduino

--- arduinoController1_1.py
+++ arduinoController1_1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from email import header
 from fileinput import filename
 import serial
 from datetime import datetime
@@ -37,6 +38,7 @@ class StabilitySetup:
     #     """
     #     self.ser = serial.Serial(COM, SERIAL_BAUD_RATE, timeout=1)
     #     self.ser.flush()
+
     def __init__(self) -> None:
         pass
 
@@ -92,7 +94,7 @@ class StabilitySetup:
             0.03 V
         SCAN_READ_COUNT : int
             how many times per voltage step the setup takes a voltage reading
-            3
+            3ser.
         SCAN_RATE : int
             rate at which scan will progress
             100 mV/s
@@ -118,14 +120,27 @@ class StabilitySetup:
 
         self.parameters = "<scan," + str(SCAN_RANGE) + "," + str(SCAN_STEP_SIZE) + "," + str(SCAN_READ_COUNT) + "," + str(SCAN_RATE) + "," + str(LIGHT_STATUS) + ">"
 
-        self.arr = np.empty([6, 18], dtype="object")
-        self.arr[0][0] = "Voltage Range: " + str(SCAN_RANGE)
-        self.arr[1][0] = "Voltage Step Size: " + str(SCAN_STEP_SIZE)
-        self.arr[2][0] = "Voltage Read Count: " + str(SCAN_READ_COUNT)
-        self.arr[3][0] = "Voltage Delay Time: " + str(SCAN_RATE)
-        self.arr[4][0] = "Light Status: " + light
-        self.arr[5] = ["Volts_output", "Pixel 0 V", "Pixel 0 mA","Pixel 1 V", "Pixel 1 mA","Pixel 2 V", "Pixel 2 mA","Pixel 3 V", "Pixel 3 mA","Pixel 4 V", "Pixel 4 mA","Pixel 5 V", "Pixel 5 mA","Pixel 6 V", "Pixel 6 mA","Pixel 7 V", "Pixel 7 mA", "Time"]
 
+        headerArr = ["Volts_output",
+                       "Pixel 0 V", "Pixel 0 mA",
+                       "Pixel 1 V", "Pixel 1 mA",
+                       "Pixel 2 V", "Pixel 2 mA",
+                       "Pixel 3 V", "Pixel 3 mA",
+                       "Pixel 4 V", "Pixel 4 mA",
+                       "Pixel 5 V", "Pixel 5 mA",
+                       "Pixel 6 V", "Pixel 6 mA",
+                       "Pixel 7 V", "Pixel 7 mA",
+                       "Time"]
+
+        self.arr = np.empty([6, len(headerArr)], dtype="object")
+        self.arr[0][0], self.arr[0][1] = "Voltage Range: ", SCAN_RANGE
+        self.arr[1][0], self.arr[1][1] = "Voltage Step Size: ", SCAN_STEP_SIZE
+        self.arr[2][0], self.arr[2][1] = "Voltage Read Count: " , SCAN_READ_COUNT
+        self.arr[3][0], self.arr[3][1] = "Voltage Delay Time: ", SCAN_RATE
+        self.arr[4][0], self.arr[4][1] = "Light Status: ",  light
+        self.arr[5] = headerArr
+
+        self.ser.write(self.parameters.encode())  # send data to arduino
         self._readData()
         print(self.arr)
         self.printTime()
@@ -162,13 +177,29 @@ class StabilitySetup:
 
         self.parameters = "<PnO," + str(PNO_STARTING_VOLTAGE) + "," + str(PNO_STEP_SIZE) + "," + str(PNO_MEASUREMENTS_PER_STEP) + "," + str(PNO_MEASUREMENT_DELAY) + "," + str(DUMMY) + ">"
 
-        self.arr = np.empty([5, 26], dtype="object")
-        self.arr[0][0] = "Voltage Range: " + str(PNO_STARTING_VOLTAGE)
-        self.arr[1][0] = "Voltage Step Size: " + str(PNO_STEP_SIZE)
-        self.arr[2][0] = "Voltage Read Count: " + str(PNO_MEASUREMENTS_PER_STEP)
-        self.arr[3][0] = "Voltage Delay Time: " + str(PNO_MEASUREMENT_DELAY)
-        self.arr[4] = ["Time","Pixel 0 V","Pixel 0 mA","Pixel 1 V","Pixel 1 mA","Pixel 2 V","Pixel 2 mA","Pixel 3 V","Pixel 3 mA","Pixel 4 V","Pixel 4 mA","Pixel 5 V","Pixel 5 mA","Pixel 6 V","Pixel 6 mA","Pixel 7 V","Pixel 7 mA", "Pixel 0 PCE", "Pixel 1 PCE", "Pixel 2 PCE", "Pixel 3 PCE", "Pixel 4 PCE", "Pixel 5 PCE", "Pixel 6 PCE", "Pixel 7 PCE", "Filler Val"]
+        headerArr  = ["Time",
+                       "Pixel 0 V","Pixel 0 mA",
+                       "Pixel 1 V","Pixel 1 mA",
+                       "Pixel 2 V","Pixel 2 mA",
+                       "Pixel 3 V","Pixel 3 mA",
+                       "Pixel 4 V","Pixel 4 mA",
+                       "Pixel 5 V","Pixel 5 mA",
+                       "Pixel 6 V","Pixel 6 mA",
+                       "Pixel 7 V","Pixel 7 mA",
+                       "Pixel 0 PCE", "Pixel 1 PCE",
+                       "Pixel 2 PCE", "Pixel 3 PCE",
+                       "Pixel 4 PCE", "Pixel 5 PCE",
+                       "Pixel 6 PCE", "Pixel 7 PCE",
+                       "Filler Val"]
 
+        self.arr = np.empty([5, len(headerArr)], dtype="object")
+        self.arr[0][0], self.arr[0][1] = "Voltage Range: ", PNO_STARTING_VOLTAGE
+        self.arr[1][0], self.arr[1][1] = "Voltage Step Size: ", PNO_STEP_SIZE
+        self.arr[2][0], self.arr[2][1] = "Voltage Read Count: ", PNO_MEASUREMENTS_PER_STEP
+        self.arr[3][0], self.arr[3][1] = "Voltage Delay Time: ", PNO_MEASUREMENT_DELAY
+        self.arr[4] = headerArr
+
+        self.ser.write(self.parameters.encode())  # send data to arduino
         self._readData()
         print(self.arr)
         self.printTime()
